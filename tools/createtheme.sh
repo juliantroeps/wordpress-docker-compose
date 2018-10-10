467e06c6e3a7facf59a9a3e7a1f53909e1a02c10
Julian Tröps <j.troeps@hype-media.de>
2018-10-10 17:16:09 +0200

    updated createtheme script

--- tools/createtheme.sh
+++ tools/createtheme.sh
@@ -4,21 +4,10 @@ themename=$1
 
 cd ..
 cd www/wp-content/themes/
-mkdir -p $themename/assets/css/src
-mkdir -p $themename/assets/css/vendor
-mkdir -p $themename/assets/css/dist
-mkdir -p $themename/assets/js/src
-mkdir -p $themename/assets/js/dist
-mkdir -p $themename/assets/js/vendor
+
+git clone https://juliantroeps@bitbucket.org/hypemedia/wordpress-boilerplate.git $themename
 
 cd $themename
-touch index.php style.css functions.php page.php single.php header.php footer.php 404.php
-
-cd assets/css/src
-touch main.scss
-
-cd ../../js/src
-touch main.js
-
+rm -rf .git
 
 echo "$themename created successfully!"
